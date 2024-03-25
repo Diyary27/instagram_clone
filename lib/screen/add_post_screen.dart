eb3620f73bb6117f7284260db96b377dc8464b62
unknown <naozary.diyar@gmail.com>
2024-03-25 04:15:06 +0330

    add screen ui

--- lib/screen/add_post_screen.dart
+++ lib/screen/add_post_screen.dart
@@ -1,5 +1,12 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:instagram_clone/models/user.dart';
+import 'package:instagram_clone/providers/user_provider.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/utils/utils.dart';
+import 'package:provider/provider.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({super.key});
@@ -9,67 +16,118 @@ class AddPostScreen extends StatefulWidget {
 }
 
 class _AddPostScreenState extends State<AddPostScreen> {
+  Uint8List? _file;
+
+  // select image dialogue box
+  selectImage(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            title: Text("Create a post"),
+            contentPadding: EdgeInsets.all(20),
+            children: [
+              SimpleDialogOption(
+                child: Text("Take a photo"),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  final Uint8List file = await pickImage(ImageSource.camera);
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                child: Text("Select from gallery"),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  final Uint8List file = await pickImage(ImageSource.gallery);
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              ),
+              SizedBox(height: 12),
+              SimpleDialogOption(
+                child: Text("Close"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.arrow_back),
-        title: Text("post to"),
-        centerTitle: false,
-        actions: [
-          TextButton(
-            onPressed: () {},
-            child: Text(
-              "post",
-              style: TextStyle(
-                color: blueColor,
-                fontWeight: FontWeight.bold,
-              ),
+    final User user = Provider.of<UserProvider>(context).getUser;
+
+    return _file == null
+        ? Center(
+            child: IconButton(
+                onPressed: () => selectImage(context),
+                icon: Icon(
+                  Icons.upload,
+                )),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              leading: Icon(Icons.arrow_back),
+              title: Text("post to"),
+              centerTitle: false,
+              actions: [
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "post",
+                    style: TextStyle(
+                      color: blueColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+              backgroundColor: mobileBackgroundColor,
             ),
-          ),
-        ],
-        backgroundColor: mobileBackgroundColor,
-      ),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                backgroundImage: AssetImage("assets/default_profile.png"),
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.4,
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Write a caption...',
-                    border: InputBorder.none,
-                  ),
-                  // maxLines: 4,
-                ),
-              ),
-              SizedBox(
-                height: 45,
-                width: 45,
-                child: AspectRatio(
-                  aspectRatio: 487 / 451,
-                  child: Container(
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                      image: AssetImage(
-                        "assets/default_profile.png",
+            body: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(user.imageUrl),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Write a caption...',
+                          border: InputBorder.none,
+                        ),
+                        // maxLines: 4,
                       ),
-                      fit: BoxFit.fill,
-                      alignment: FractionalOffset.topCenter,
-                    )),
-                  ),
+                    ),
+                    SizedBox(
+                      height: 45,
+                      width: 45,
+                      child: AspectRatio(
+                        aspectRatio: 487 / 451,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                            image: MemoryImage(_file!),
+                            fit: BoxFit.fill,
+                            alignment: FractionalOffset.topCenter,
+                          )),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+              ],
+            ),
+          );
   }
 }
